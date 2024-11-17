aac315d4017c20c39c85e90c95992e1c9bbe93a9
abhinv98 <98abrai@gmail.com>
2024-11-18 02:03:04 +0530

    analytics packages added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics debug={process.env.NODE_ENV === "development"} />
+    <SpeedInsights debug={process.env.NODE_ENV === "development"} />
   </React.StrictMode>
 );
 
